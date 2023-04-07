6d51dbdbdab461855ab31d78eb944e518fcbb28c
chengtianhao.cc <chengtianhao.cc@bytedance.com>
2023-04-07 15:40:35 +0800

    random port

--- examples/pytorch/gpt/utils/gpt.py
+++ examples/pytorch/gpt/utils/gpt.py
@@ -529,6 +529,7 @@ class GPT(nn.Module):
         os.environ["WORLD_SIZE"] = '1'
         os.environ["MASTER_ADDR"] = '127.0.0.1'
         os.environ['MASTER_PORT'] = os.environ.get("PORT1",'27111')
+        print(f"address:{os.environ['MASTER_ADDR']}, port:{os.environ['MASTER_PORT']}")
         dist.init_process_group(backend='nccl')
         self.rank = dist.get_rank()
         self.device_count = torch.cuda.device_count()
